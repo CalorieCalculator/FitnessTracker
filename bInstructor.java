5532b46984121c3ce76d8e5991a8712d3c5c8419
ldamte21 <57686774+ldamte21@users.noreply.github.com>
2019-12-03 15:46:05 -0500

    Add files via upload

--- bInstructor.java
+++ bInstructor.java
@@ -1,26 +1,38 @@
-import javax.swing.*;
-import java.awt.*;
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JTextArea;
+import javax.swing.border.EmptyBorder;
 
 public class bInstructor extends JFrame {
-    private JFrame instructor;
-    private JButton loseWeight,gainweight,maintainWeight;
-    private JTextArea bmiText1;
-    private JPanel panel;
-    public bInstructor(){
+	private JTextArea bmiText1;
+	private JPanel contentPane;
 
-        bmiText1 = new JTextArea("BMI VALUES\n Underweight: Under 18.5\n Normal: 18.5-24.9\n Overweight: 25-29.9\n Obese: 30 or over");
-        loseWeight = new JButton("Lose");
-        gainweight = new JButton("Gain");
-        maintainWeight = new JButton("Maintain");
-        panel = new JPanel();
-        setBounds(100, 100, 450, 500);
-        bmiText1.setEditable(false);
-        panel.setLayout(new BorderLayout());
-        panel.add(bmiText1,BorderLayout.NORTH);
-        panel.add(loseWeight,BorderLayout.CENTER);
-        panel.add(gainweight,BorderLayout.CENTER);
-        panel.add(maintainWeight,BorderLayout.CENTER);
-        add(panel);
-    }
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					bInstructor frame = new bInstructor();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public bInstructor() {
+		bmiText1 = new JTextArea("BMI VALUES\n Underweight: Under 18.5\n Normal: 18.5-24.9\n Overweight: 25-29.9\n Obese: 30 or over");
+        
+	}
 
 }
